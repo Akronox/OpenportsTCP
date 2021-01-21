5527a2654bc8883a595502884544712847587ec4
Akronox <65143783+Akronox@users.noreply.github.com>
2021-01-21 17:50:35 +0000

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -5,8 +5,8 @@
 if [ $1 ]; then
     ip_address=$1
     for port in $(seq 1 65535); do 
-        timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" 2>/dev/null && echo "puerto abierto" &
-    done
+        timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" 2>/dev/null && echo "[+] Port $port - puerto abierto" &
+    done; wait
 else
     echo -e "\n[+] Uso: ./portscan.sh <ip_address>\n"
     
