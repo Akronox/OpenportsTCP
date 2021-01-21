7a86c4372ec5951af2fc92eeb9302c6066ba20b9
Akronox <65143783+Akronox@users.noreply.github.com>
2021-01-21 17:38:30 +0000

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -8,7 +8,7 @@ if [ $1 ]; then
         timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" && echo "puerto abierto" &
     done
 else
-    echp -e "\n[+] Uso: ./portscan.sh <ip_address>\n"
+    echo -e "\n[+] Uso: ./portscan.sh <ip_address>\n"
     
     echo "echo por Akronox (Ctrl +c para cortar)"
         exit
