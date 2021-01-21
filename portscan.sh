bc3a183e75ae690385215e629d94d27d72acfda1
Akronox <65143783+Akronox@users.noreply.github.com>
2021-01-21 17:46:04 +0000

    Update portscan.sh

--- portscan.sh
+++ portscan.sh
@@ -5,11 +5,11 @@
 if [ $1 ]; then
     ip_address=$1
     for port in $(seq 1 65535); do 
-        timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" && echo "puerto abierto" &
+        timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" 2>/dev/null && echo "puerto abierto" &
     done
 else
     echo -e "\n[+] Uso: ./portscan.sh <ip_address>\n"
     
     echo "echo por Akronox (Ctrl +c para cortar)"
-        exit
+        exit 1
 fi
